e8671c151305f749e7ad91cb23b74f428cb79511
monzuralam <monzur707@gmail.com>
2024-04-27 17:47:48 +0600

    init

---
+++ app/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="max-w-md max-h-screen mx-auto overflow-y relative ">
+      <div className="container h-screen overflow-y-scroll py-16 bg-white dark:bg-black">
+        <h1 class="text-3xl bold text-center">বাংলাদেশ ট্রেনের সময়সুচি</h1>
+      </div>
+    </main>
+  );
+}
